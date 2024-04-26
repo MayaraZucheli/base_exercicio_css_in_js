74dbac875a4a489070d77bb2646553676b028232
MayaraZucheli <mayara.zucheli@gmail.com>
2024-04-26 15:21:22 -0300

    comitando styled components

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,19 @@
-import styles from './Cabecalho.module.css'
+import React from 'react'
+import styled from 'styled-components'
 
-const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
-)
+const StyledHeader = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
+const Cabecalho = () => {
+  return (
+    <StyledHeader>
+      <h1>EBAC Jobs</h1>
+    </StyledHeader>
+  )
+}
 
 export default Cabecalho
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,50 @@
-import styles from './Hero.module.css'
+import React from 'react'
+import styled from 'styled-components'
 
-const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
-)
+const StyledHero = styled.div`
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  height: 360px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--cor-principal);
+    content: '';
+    opacity: 0.7;
+  }
+
+  h1 {
+    position: relative;
+    color: #eee;
+    font-family: Gloock, serif;
+    font-size: 48px;
+    text-align: center;
+  }
+
+  @media (max-width: 768px) {
+    height: auto;
+    padding: 24px 0;
+
+    h1 {
+      font-size: 32px;
+    }
+  }
+`
+
+const Hero = () => {
+  return (
+    <StyledHero>
+      <h1>As melhores vagas para tecnologia, design e artes visuais.</h1>
+    </StyledHero>
+  )
+}
 
 export default Hero
